915ecf2181b158287c47ed82a958cf8721cf700b
nvmxm5 <Ma.Danica.Mora@retailbusinessservices.com>
2026-04-03 18:12:12 +0800

    adding base url to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "moradanica.github.io",
+  base: "https://github.com/Moradanica/moradanica.github.io",
 });
