0b05c07ec988fec588b8d1774adb63d5125554d0
nvmxm5 <Ma.Danica.Mora@retailbusinessservices.com>
2026-04-04 16:43:34 +0800

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const navItems = ["Projects", "Skills", "About", "Contact"];
   return (
     <nav className="flex flex-col md:flex-row justify-between items-center fixed top-0 right-0 left-0 mx-20 h-17 bg-transparent backdrop-blur-xs p-3">
-      <div className="hidden lg:block raleway-font font-bold text-2xl uppercase w-1/4">
+      <div className="hidden md:block raleway-font font-bold text-2xl uppercase w-1/4">
         Portfolio
       </div>
 
